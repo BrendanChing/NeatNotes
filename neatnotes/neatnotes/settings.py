0854ae6b7651f193e3e2fe890a5214406802c4fa
Brendan Ching <brendanching1@gmail.com>
2024-07-03 13:01:34 +0000

    setup base.html including bootstrap link

--- neatnotes/neatnotes/settings.py
+++ neatnotes/neatnotes/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'neatnotes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            BASE_DIR / 'static/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,7 +126,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- neatnotes/notes/views.py
+++ neatnotes/notes/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from .models import Notes
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 class NotesListView(ListView):
     model = Notes
     context_object_name = "notes"
 
-def detail(request, pk):
-    note = Notes.objects.get(pk=pk)
-    return render(request, 'notes/notes_detail.html', {'note': note})
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"    
