a24f64aae5b950ebc9721cb4020e30e550e8d1ef
Brendan Ching <brendanching1@gmail.com>
2024-07-03 17:27:40 +0000

    login requirement for view class

--- neatnotes/notes/views.py
+++ neatnotes/notes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Notes
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from . forms import NotesForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 class NotesDeleteView(DeleteView):
@@ -20,9 +21,13 @@ class NotesCreateView(CreateView):
     success_url = '/smart/notes/'
     form_class = NotesForm
 
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = "notes"
+    login_url = "/admin"
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 class NotesDetailView(DetailView):
     model = Notes
