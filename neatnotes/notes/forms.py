d1a38fbca15fb563445d403a14d5ebd1c9f541a1
Brendan Ching <brendanching1@gmail.com>
2024-07-03 16:13:17 +0000

    added form.py file and validation for user input

---
+++ neatnotes/notes/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Notes
+
+class NotesForm(forms.ModelForm):
+    class Meta:
+        model = Notes
+        fields = ('title', 'text')
+
+def clean_title(self):
+    title  = self.cleaned_data['title']
+    if 'Django' not in title:
+        raise forms.validationError('We only accept notes about Django')
+    return title            
