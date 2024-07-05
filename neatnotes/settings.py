da2a57d8cd35614a435bb0d168ae1dce93f632ba
Brendan Ching <brendanching1@gmail.com>
2024-07-05 12:35:46 +0000

    added login_required_url to settings

--- neatnotes/settings.py
+++ neatnotes/settings.py
@@ -154,3 +154,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REQUIRED_URL = '/smart/notes'
