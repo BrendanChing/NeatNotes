20618765dcc6651457531f2456a2dd4f7df413ae
Brendan Ching <brendanching1@gmail.com>
2024-07-05 12:43:12 +0000

    created logout view and added to urlpatterns

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('authorized/', views.AuthorizedViews.as_view(), name='authorized'),
-    path('login/', views.LoginInterfaceView.as_view())
+    path('login/', views.LoginInterfaceView.as_view()),
+    path('logout/', views.LogoutInterfaceView.as_view())
 ]
--- home/views.py
+++ home/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 # Create your views here.
 class LoginInterfaceView(LoginView):
     template_name = 'home/login.html'
+
+class LogoutInterfaceView(LogoutView):
+    template_name = 'home/logout.html'        
 
 class HomeView(TemplateView):
     template_name = 'home/welcome.html'
